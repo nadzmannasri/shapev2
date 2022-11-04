89d4451b5ccb335013be871f2465a442a59ff042
nadzmannasri <116859531+nadzmannasri@users.noreply.github.com>
2022-11-04 11:45:34 +0800

    Add files via upload

---
+++ shapev2/shape/shape/square.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    class square : rectangle
+    {
+        private double _len;
+        public double Len
+        {
+            get { return _len; }
+            set { _len = value; }
+        }
+
+        public square()
+        {
+            _len = 0;
+        }
+
+        public square(double len)
+        {
+            _len = len;
+        }
+
+        public new double area()
+        {
+            return _len*_len;
+        }
+
+        public new double circumference()
+        { 
+            return 4 * _len;
+        }
+    }
+}
